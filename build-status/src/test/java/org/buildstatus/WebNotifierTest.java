2fa6bc913b39ea61dd98808cf42cdcd1614511db
paulgreg <paulgreg@6a67fcbe-2a86-11de-906c-4b287f8c8247>
2009-04-18 13:02:43 +0000

    Adding more tests

--- build-status/src/test/java/org/buildstatus/WebNotifierTest.java
+++ build-status/src/test/java/org/buildstatus/WebNotifierTest.java
@@ -1,6 +1,11 @@
 package org.buildstatus;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,11 +19,12 @@ import org.junit.Test;
 
 public class WebNotifierTest {
 
+	private static final String TEST_APP_NAME = "junit-test";
 	private WebNotifier notifier;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		
+
 		NotificatorRegistry mockedRegistry = new NotificatorRegistry() {
 
 			public Notificator findNotificator(String arg0) {
@@ -37,18 +43,59 @@ public class WebNotifierTest {
 
 			public void unregister(Notificator arg0) {
 			}
-			
+
 		};
-		
+
 		notifier = new WebNotifier(mockedRegistry);
 	}
 
 	@Test
-	public void notifyBuildStatusAppPass() {
+	public void notifyBuildStatusAppPass() throws MalformedURLException, IOException {
 		try {
-			notifier.notifyBuildStatusApp(WebNotifier.PASS, "junit-test");
+			notifier.notifyBuildStatusApp(WebNotifier.PASS, TEST_APP_NAME);
 		} catch (Exception e) {
 			Assert.fail(e.getMessage());
 		}
+
+		Assert.assertTrue(isThatStringPresentOnAppEngineTestPage("favicon-pass.png"));
+	}
+
+	@Test
+	public void notifyBuildStatusAppFail() throws MalformedURLException,
+			IOException {
+		try {
+			notifier.notifyBuildStatusApp(WebNotifier.FAIL, TEST_APP_NAME);
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+
+		Assert.assertTrue(isThatStringPresentOnAppEngineTestPage("favicon-fail.png"));
+	}
+
+	/**
+	 * Search the corresponding string in the AppEngine "junit-test" web page
+	 * @param searchedString looking for String
+	 * @return true if found, false otherwise
+	 * @throws IOException
+	 * @throws MalformedURLException
+	 */
+	private boolean isThatStringPresentOnAppEngineTestPage(String searchedString) throws IOException,
+			MalformedURLException {
+		InputStream is = new URL(WebNotifier.BASE_URL
+				+ WebNotifier.APP_NAME_PARAM + TEST_APP_NAME).openStream();
+
+		boolean faviconFound = false;
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.indexOf(searchedString) != -1) {
+				faviconFound = true;
+				break;
+			}
+		}
+
+		return faviconFound;
 	}
 }
